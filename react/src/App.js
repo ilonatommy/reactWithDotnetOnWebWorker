7257a876fe30c4eabf486af7a486ef6a4da3b06e
Ilona Tomkowicz <itomkowicz@microsoft.com>
2024-01-09 08:28:26 +0100

    Cleanup after: remove spinning react logo.

--- react/src/App.js
+++ react/src/App.js
@@ -1,6 +1,5 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 import { Popup } from './Popup.js';
